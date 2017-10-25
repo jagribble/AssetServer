1952d382f6700cf6e2a8d1703f584f6aaf3fea6a
Jules Gribble <gribblej@me.com>
2017-10-25 11:07:38 +0100

    added postgres connection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const { Client } = require('pg');
 
 const router = express.Router();
+
+// connect to the postgres database
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+
+/**
+Create database tables
+
+CREATE TABLE Asset(
+  assetID SERIAL PRIMARY KEY,
+  assetName VARCHAR(50).
+  assetX FLOAT(13),
+  assetY FLOAT(13),
+);
+* */
+
+client.connect();
 
 router.get('/', (req, res) => {
   // initialise empty array for results of db query
