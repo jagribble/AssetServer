45ea425a42c5340efe69dc2cb0244cd3e97ea896
Jules Gribble <gribblej@me.com>
2017-11-10 09:50:32 +0000

    endpoints to match week 6

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,13 @@ CREATE TABLE Asset(
   assetName VARCHAR(50).
   assetX FLOAT(13),
   assetY FLOAT(13),
+  orginizationID BIGINT FORIEGN KEY,
 );
 
+CREATE TABLE orginization(
+  orginizationID SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+);
 
 * */
 
@@ -29,15 +34,23 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+router.get('/change', (req, res) => {
+  // initialise empty array for results of db query
+  client.query('DELETE FROM Asset').then((result) => {
+    res.send(result);
+  });
+  // res.render('index');
+});
+
 /*
 INSERT asset into database
 
 INSERT INTO Asset (assetName, assetX, assetY)
 VALUES ('testAsset', 51.34343, 0.923132);
 */
-router.post('/insert', (req, res) => {
+router.post('/insert/asset', (req, res) => {
   console.log(req.body.name);
-  client.query(`INSERT INTO Asset (assetName, assetX, assetY) VALUES ('${req.body.name}', ${req.body.assetX}, ${req.body.assetY})`)
+  client.query(`INSERT INTO Asset (assetName, assetX, assetY,orginizationID) VALUES ('${req.body.name}', ${req.body.assetX}, ${req.body.assetY}, ${req.body.orginizationID})`)
     .then((result) => {
       console.log(result.rows[0]);
       res.send(result);
@@ -48,12 +61,45 @@ router.post('/insert', (req, res) => {
     });
 });
 
+// SELECT * FROM orginization'
+// ALTER TABLE orginization RENAME TO Orginization
+router.get('/:orginization/assets', (req, res) => {
+  console.log(`SELECT * FROM Asset WHERE orginizationID=(SELECT orginizationID FROM orginization WHERE name='${req.params.orginization}')`);
+  client.query(`SELECT * FROM Asset WHERE orginizationID=(SELECT orginizationID FROM orginization WHERE name='${req.params.orginization}')`).then((result) => {
+    res.send(result);
+  });
+});
 
-router.get('/selectAsset', (req, res) => {
+router.post('/insert/orginization', (req, res) => {
+  console.log(req.body.name);
+  client.query(`INSERT INTO orginization (name) VALUES ('${req.body.name}')`)
+    .then((result) => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch((e) => {
+      console.error(e.stack);
+      res.send(e);
+    });
+});
+
+router.get('/assets', (req, res) => {
   client.query('SELECT * FROM Asset')
     .then((result) => {
-      console.log(result.rows[0]);
-      res.send(result.rows);
+      console.log(result);
+      res.send(result);
+    })
+    .catch((e) => {
+      console.error(e.stack);
+      res.send(e);
+    });
+});
+
+router.get('/orginization', (req, res) => {
+  client.query('SELECT * FROM orginization')
+    .then((result) => {
+      console.log(result);
+      res.send(result);
     })
     .catch((e) => {
       console.error(e.stack);
