cb85ba91467adb1ba53f802446f5599a26e8870a
Jules Gribble <gribblej@me.com>
2017-11-10 15:25:07 +0000

    added more ReSTful calls

--- routes/index.js
+++ routes/index.js
@@ -25,21 +25,44 @@ CREATE TABLE orginization(
   name VARCHAR(50),
 );
 
+Using data examples from https://www.gouldspumps.com/ittgp/medialibrary/goulds/website/Literature/White%20Papers/smart_pumping_system.pdf?ext=.pdf
+CREATE TABLE Data(
+  dataID SERIAL PRIMARY KEY,
+  dataName VARCHAR(50),
+  dataUnit VARCHAR(10),
+  assetID INT FORIEGN KEY
+);
+
+CREATE TABLE DataPoint(
+  dataPointID SERIAL PRIMARY KEY,
+  timeStamp TIMESTAMP,
+  data VARCHAR(50),
+  dataID INT,
+)
 * */
 
 client.connect();
 
 router.get('/', (req, res) => {
-  // initialise empty array for results of db query
   res.render('index');
 });
 
 router.get('/change', (req, res) => {
-  // initialise empty array for results of db query
-  client.query('DELETE FROM Asset').then((result) => {
+  client.query('CREATE TABLE DataPoint(dataPointID SERIAL PRIMARY KEY,timeStamp TIMESTAMP,data VARCHAR(50),dataID INT)').then((result) => {
     res.send(result);
   });
-  // res.render('index');
+});
+
+router.get('/data', (req, res) => {
+  client.query('SELECT * FROM Data').then((result) => {
+    res.send(result);
+  });
+});
+
+router.get('/datapoints', (req, res) => {
+  client.query('SELECT * FROM DataPoint').then((result) => {
+    res.send(result);
+  });
 });
 
 /*
@@ -48,27 +71,52 @@ INSERT asset into database
 INSERT INTO Asset (assetName, assetX, assetY)
 VALUES ('testAsset', 51.34343, 0.923132);
 */
+router.post('/:orginization/insert/asset', (req, res) => {
+  client.query(`SELECT orginizationID FROM orginization WHERE name='${req.params.orginization}'`).then((organization) => {
+    console.log(organization);
+    client.query(`INSERT INTO Asset (assetName, assetX, assetY,orginizationID) VALUES ('${req.body.name}', ${req.body.assetX}, ${req.body.assetY}, ${organization.rows[0].orginizationid})`)
+      .then((result) => {
+        console.log(result.rows[0]);
+        res.send(result);
+      });
+  });
+});
+
 router.post('/insert/asset', (req, res) => {
-  console.log(req.body.name);
   client.query(`INSERT INTO Asset (assetName, assetX, assetY,orginizationID) VALUES ('${req.body.name}', ${req.body.assetX}, ${req.body.assetY}, ${req.body.orginizationID})`)
     .then((result) => {
       console.log(result.rows[0]);
       res.send(result);
-    })
-    .catch((e) => {
-      console.error(e.stack);
-      res.send(e);
     });
 });
 
-// SELECT * FROM orginization'
-// ALTER TABLE orginization RENAME TO Orginization
+router.post('/intert/asset/:assetID/data', (req, res) => {
+  client.query(`INSERT INTO Data (dataName, dataUnit, assetID) VALUES ('${req.body.name}','${req.body.unit}',${req.params.assetID})`).then((result) => {
+    res.send(result);
+  });
+});
+
+router.post('/insert/asset/:assetID/data/:dataID/datapoint', (req, res) => {
+  client.query(`INSERT INTO DataPoint (timeStamp, data,dataID) VALUES (${req.body.time},${req.body.data},${req.params.dataID})`).then((result) => {
+    res.send(result);
+  });
+});
+/*
+Get all assets from an organization
+*/
 router.get('/:orginization/assets', (req, res) => {
   console.log(`SELECT * FROM Asset WHERE orginizationID=(SELECT orginizationID FROM orginization WHERE name='${req.params.orginization}')`);
   client.query(`SELECT * FROM Asset WHERE orginizationID=(SELECT orginizationID FROM orginization WHERE name='${req.params.orginization}')`).then((result) => {
     res.send(result);
   });
 });
+
+router.get('/:orginization/asset/:id', (req, res) => {
+  client.query(`SELECT * FROM Asset WHERE assetID=${req.params.id}`).then((result) => {
+    res.send(result);
+  });
+});
+
 
 router.post('/insert/orginization', (req, res) => {
   console.log(req.body.name);
@@ -82,6 +130,7 @@ router.post('/insert/orginization', (req, res) => {
       res.send(e);
     });
 });
+
 
 router.get('/assets', (req, res) => {
   client.query('SELECT * FROM Asset')
